b795418c9439c9270b4d9312b6b36c072fbf67ce
Giona Fossati <fossatigiona@gmail.com>
2020-02-17 22:38:05 +0100

    reverted to yesterday at 3am

--- script.js
+++ script.js
@@ -25,67 +25,55 @@ function getY(a){
     var y = (element.top+element.bottom)/2;
     return parseInt(y);
 }
-function removeEmpty(received){
+
+function displayConnections(id0, id1, id2, id3, id4, id5) {
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"];
+    var array = [];
     var array2 = [];
+    var received = [id0, id1, id2, id3, id4, id5];
+    console.log(received);
     for(a = 0; a<received.length; a++){
         if(isNaN(received[a])==false){
             array2.push(received[a]);
         }
     }
-    return array2;
-}
-function addLinkedScenes(received){
-    var allClass2 = [];
-    var allClass = document.querySelectorAll("[class ='"+received[0]+"']");
-    allClass.forEach(e => { allClass2.push(e.childNodes); });
-    for(a = 0; a<allClass2.length; a++){
-        var circle = allClass2[a][1];
-        if(a>0){
-            circle2 = allClass2[a-1][1];
-            if(circle.id.substring(1) != circle2.id.substring(1)){ // removing duplicates
-                received.push(parseInt(circle.id.substring(1)));
+    console.log(array2);
+    if(document.querySelector("#t"+id0).firstChild){
+        console.log("yes",  array2.length);
+        for(a=0; a<array2.length; a++){
+            var x = document.querySelector("#t"+array2[a]).firstChild;
+            console.log(x);
+            if(x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
             }
-        } else {
-            received.push(parseInt(circle.id.substring(1)));
         }
-    }
-}
-function changeDisplay(x){
-    if(x.style.display === "none") {
-        x.style.display = "block";
+        array2.sort();
+        var y = document.getElementById(array2[0].toString().concat(array2[array2.length-1]));
+        console.log(y);
+        if(y.style.display === "none") {
+            y.style.display = "block";
+        } else {
+            y.style.display = "none";
+        }
     } else {
-        x.style.display = "none";
-    }
-}
-function changeElementVisibility(received) {
-    for(a=0; a<received.length; a++){
-        var x = document.querySelector("#t"+received[a]).firstChild;
-        changeDisplay(x)
-    }
-    received.sort();
-    var y = document.getElementById(received[0].toString().concat(received[received.length-1]));
-    changeDisplay(y)
-}
-function displayConnections(id0, id1, id2, id3, id4, id5) {
-    var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"];
-    var received = [id0, id1, id2, id3, id4, id5];
-    received = removeEmpty(received);
-    addLinkedScenes(received);
-
-    if(document.querySelector("#t"+id0).firstChild){ //if exists in DOM
-        changeElementVisibility(received);
-    } else { //if doesn't exist in DOM
-        var array = [];
-        for(b = 0; b<received.length; b++){
+        console.log("no");
+        var numbers = [];
+        for(a = 0; a<received.length; a++){
+            if(received[a]>0){
+                numbers.push(received[a]);
+            }
+        }
+        for(b = 0; b<numbers.length; b++){
             for(a = 0; a<alphabet.length; a++){
-                var test = alphabet[a] + received[b];
+                var test = alphabet[a] + numbers[b];
                 if(document.getElementById(test)!==null){
                     array.push(test);
                     break;
                 }
             }
         }
-
         for(a=0; a<array.length; a++){
                 getX(array[a]);
                 createCircle(array[a], "ex"+array[a].substring(1));
@@ -93,28 +81,28 @@ function displayConnections(id0, id1, id2, id3, id4, id5) {
         array.sort(function(a, b){
             return a.substring(1, a.lenght) - b.substring(1, b.length);
         });
-
         var x1 = getX(array[0]);
         var x2 = getX(array[array.length-1]);
+        //x1.round();
+        //x2.round();
         createLineX(x1, x2, array[0].substring(1), array[array.length-1].substring(1));
     }
+
+    /*
+  var x = document.getElementById(id1);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+*/
 }
 function getX(a){
     var el = document.getElementById(a);
-    var element =el.getBoundingClientRect();
-    var x = (element.left+element.right)/2+window.pageXOffset;
+    var element = el.getBoundingClientRect();
+    var x = (element.left+element.right)/2;
     return parseInt(x);
-}
-function onBodyScroll(){
-    var x = document.getElementById("titles");
-    var offset = window.pageYOffset;
-    var top = parseInt(window.getComputedStyle(x).getPropertyValue("top"));
-    if(top < 266){
-        var new_top = 50-offset;
-    } else {
-        var new_top = 50+offset;
-    }
-    x.style.top = new_top+"px";
 }
 function createCircle(a, b){
     var myCircle = document.createElementNS("http://www.w3.org/2000/svg","circle"); //to create a circle. for rectangle use "rectangle"
@@ -133,9 +121,9 @@ function createCircle(a, b){
 function createLineX(x1, x2, n1, n2){
     var myLine = document.createElementNS("http://www.w3.org/2000/svg","line"); //to create a circle. for rectangle use "rectangle"
     myLine.setAttribute("x1",x1);
-    myLine.setAttribute("y1",20);
+    myLine.setAttribute("y1",37);
     myLine.setAttribute("x2",x2);
-    myLine.setAttribute("y2",20);
+    myLine.setAttribute("y2",37);
     myLine.setAttribute("id", n1+n2);
 
     document.getElementById("big").appendChild(myLine);
@@ -143,9 +131,9 @@ function createLineX(x1, x2, n1, n2){
 function createLineY(y1, y2, x1, n1, n2){
     var myLine = document.createElementNS("http://www.w3.org/2000/svg","line"); //to create a circle. for rectangle use "rectangle"
     myLine.setAttribute("x1",x1);
-    myLine.setAttribute("y1",y1+window.pageYOffset);
+    myLine.setAttribute("y1",y1);
     myLine.setAttribute("x2",x1);
-    myLine.setAttribute("y2",y2+window.pageYOffset);
+    myLine.setAttribute("y2",y2);
     myLine.setAttribute("id", n1+n2);
 
     document.getElementById("big").appendChild(myLine);
